4a1ed55cfddd69ebfc81080a28d640ea54225da9
Vadim <litvinenkov7@gmail.com>
2022-06-25 00:45:37 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -14,4 +14,27 @@ $(document).ready(function () {
          }
       ]
    });
+
+   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function () {
+      $(this)
+         .addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
+         .closest('div.container').find('div.catalog__items').removeClass('catalog__items-active').eq($(this).index()).addClass('catalog__items-active');
+   });
+
+   $('.catalog__link').each(function (i) {
+      $(this).on('click', function (e) {
+         e.preventDefault();
+         $('.catalog__content').eq(i).togleClass('catalog__content-active');
+         $('.catalog__list').eq(i).toggleClass('catalog__list-active');
+      })
+   });
+
+   $('.catalog__list-link').each(function (i) {
+      $(this).on('click', function (e) {
+         e.preventDefault();
+         $('.catalog__content').eq(i).togleClass('catalog__content-active');
+         $('.catalog__list').eq(i).toggleClass('catalog__list-active');
+      })
+   });
+
 });
