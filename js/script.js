4be1475b9df2b4469bcf3cce66ebb00566013c38
Vadim <litvinenkov7@gmail.com>
2022-06-24 18:48:42 +0300

    Sliders

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+   $('.courusel__slick').slick({
+      nextArrow: '<button type="button" class="slick-next"><img src="images/icon/right.png"></button>',
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/icon/left.png"></button>',
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               arrows: false,
+               slidesToScroll: 1,
+               infinite: true,
+            }
+         }
+      ]
+   });
+});
